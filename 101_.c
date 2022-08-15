4b4fc84c435c24575b1051002bb21c3c1be19a3a
Aaron Moreno Villeda <hh1360@hotmail.com>
2022-08-14 20:06:58 -0500

    Create 101_.c

---
+++ 101_.c
@@ -0,0 +1,16 @@
+/*
+*Desarrollador: Aaron Moreno Villeda
+*
+*Fecha de creación: 14/08/22
+*
+*1.-Desarrollar un programa que permita el ingreso de dos matrices cuadradas nxn (A y B) y que se efectuen operaciones sobre ellas, la suma A+B, resta A-B, 
+*multiplicación A*B, así como la obtención del determinante.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+  
+  return 0;
+}
