90da34ca006fbed8981b51044c957bb82fb23909
Aaron Moreno Villeda <hh1360@hotmail.com>
2022-08-17 13:02:48 -0500

    ingresar matrices

--- 101_.c
+++ 101_.c
@@ -12,16 +12,40 @@
 
 int main()
 {
-  int n=1,m=1,matriz1[n],matriz2[m];
+  int n=1,m=1,matriz1[n][n],matriz2[m][m];
   
   //*********************INGRESAR MATRICES**********************************
+  //MATRIZ 1    
   printf("Tamaño de la primer matriz: ");
   scanf("%d",&n);
     system("clear");
+  
+  for(int i=0;i<n;i++)
+  {
+      for(int j=0;j<n;j++)
+      {
+          printf("M1[%d][%d]= ",i,j);
+          scanf("%d",&matriz1[i][j]);
+      }
+  }
+    system("clear");
+
+  //MATRIZ 2    
   printf("Tamaño de la segunda matriz: ");
   scanf("%d",&m);
     system("clear");
   
+  for(int i=0;i<m;i++)
+  {
+      for(int j=0;j<m;j++)
+      {
+          printf("M2[%d][%d]= ",i,j);
+          scanf("%d",&matriz2[i][j]);
+      }
+  }
+     system("clear");
+     
+  //*********************IMPRIMIR MATRICES**********************************
   
   return 0;
 }
