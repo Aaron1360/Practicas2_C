0cf6e3a80c1646ac3c3560d0d23c57f7e4396fce
Aaron Moreno Villeda <hh1360@hotmail.com>
2022-08-17 12:39:59 -0500

    ingresar tamaño de la matriz

--- 101_.c
+++ 101_.c
@@ -10,61 +10,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int * ingresarMatriz(int n);
-
-//TODO: ARREGLAR FUNCION imprimirMatriz
-
-void imprimirMatriz(int * matriz);
-
 int main()
 {
-  int n=0,m=0;
-  int matriz1[n],matriz2[m];
-
-  printf("Ingresa el tamaño de la primer matriz (n)>>> ");
+  int n=1,m=1,matriz1[n],matriz2[m];
+  
+  //*********************INGRESAR MATRICES**********************************
+  printf("Tamaño de la primer matriz: ");
   scanf("%d",&n);
-
-  ingresarMatriz(n);
-  
-  printf("Ingresa el tamaño de la segunda matriz (m)>>> ");
+    system("clear");
+  printf("Tamaño de la segunda matriz: ");
   scanf("%d",&m);
+    system("clear");
   
-  ingresarMatriz(m);
-
-  //system("cls");
- // imprimirMatriz(*matriz1);
-
+  
   return 0;
 }
-int * ingresarMatriz(int n)
-{
-  int matriz[n][n];//,matriz1[n];
- 
-  //INGRESAR VALORES
-  for(int i=0;i<n;i++)
-  {
-    for(int j=0;j<n;j++)
-    {
-      system("cls");
-      printf("matriz[%d][%d]= ",i,j);
-      scanf("%d",&matriz[i][j]);
-    }
-  }
-  return *matriz;
-}
-//TODO: BUSCAR COMO IMPRIMIR UNA MATRIZ CON PUNTEROS DESDE LA FUNCION 
-
-void imprimirMatriz(int * matriz)
-{
-  //IMPRIMIR VALORES
-  //TODO: DECLARAR VARIABLE PARA IMPRIMIR
-  for(int i=0;i<sizeof(matriz);i++)
-  {
-    for(int j=0;j<sizeof(matriz);j++)
-    {
-      if(j==0) printf("| ");
-      printf("%d ",matriz[i][j]);
-      if(j==-1) printf("|\n");
-    }
-    
-  }
