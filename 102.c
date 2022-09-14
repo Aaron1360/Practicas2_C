597b14f2c4a59412ea86e361ca548add2c8428a9
Aaron Moreno Villeda <hh1360@hotmail.com>
2022-09-14 09:20:26 -0500

    Update 102.c

--- 102.c
+++ 102.c
@@ -10,25 +10,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void input_array(int rows,int cols,int array[rows][cols]);
-void print_array(int rows,int cols,int array[rows][cols]);
-void mult(int rows,int cols,int array1[rows][cols],int array2[cols][1]);
+//PROTOTYPES
+void input_array(int rows,int cols,int array[rows][cols]);//ENTER VALUES
+void print_array(int rows,int cols,int array[rows][cols]);//SHOW ARRAYS
+void mult(int rows,int cols,int array1[rows][cols],int array2[cols][1]);//MULTIPLY ARRAYS
+int det(int size,int array[size][size]);//CALCULATE THE DETERMINANT
+int cof(int size,int array[size][size],int row,int col);//COFACTORS ARRAY 
+void transpose(int size,int array[size][size],int trArray[size][size]);//TRANSPOSE MATRIX
 
 int main()
-{
+{   
+    //>>>>>>>ENTER ARRYA SIZE
     int rows,cols;
     printf("enter rows and cols: \n");
     scanf("%d %d",&rows,&cols);
-    int A[rows][cols],B[cols][1];
+    int A[rows][cols],B[cols][1],tr_A[rows][cols];
     system("clear");
     
+    //>>>>>>>PRINT ARRAYS
     printf("MATRIX A:\n");
     input_array(rows,cols,A);
     system("clear");
     printf("MATRIX A:\n");
     print_array(rows,cols,A);
-    
     printf("\nMATRIX B:\n");
     input_array(cols,1,B);
     system("clear");
@@ -37,9 +43,30 @@ int main()
     printf("\nMATRIX B:\n");
     print_array(cols,1,B);
     
+    //>>>>>>>PRINT RESULTS
+    /*
     printf("\nPRODUCTO:\n");
     mult(rows,cols,A,B);
-
+    */
+    //>>>>>>>DETERMINANT(A)
+   if(rows==cols)
+   {
+        printf("\nDET(A): %d\n",det(rows,A));
+        //>>>>>>>TRANSPOSE(A)
+        if(det(rows,A) != 0 )
+        {
+            transpose(rows,A,tr_A);
+            
+        }
+        else
+        {
+            printf("\n[A]^-1 EXIST ONLY IF DET(A) != 0\n\n");
+        }
+   }
+   else
+   {
+       printf("\n\t[A] IS NOT SQUARE!\n");
+   }
     return 0;
 }
 
@@ -84,3 +111,60 @@ void mult(int rows,int cols,int array1[rows][cols],int array2[cols][1])
         printf("[%d]\n",m[i][0]);
     }
 }
+
+int det(int size,int array[size][size])
+{
+     int result=0;
+    
+    if(size==1){result=array[0][0];}
+    
+    else
+    {
+        for(int j=0;j<size;j++)
+        {
+            result+=array[0][j]*cof(size,array,0,j);
+        }
+    }
+    return result;
+}
+
+int cof(int size,int array[size][size],int row,int col)
+{
+    int orden=size-1;
+    int sub[orden][orden];
+    int x=0,y=0;
+    
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            if((i!=row) && (j!=col))
+            {
+                sub[x][y]=array[i][j];
+                y++;
+                
+                if(y>=orden)
+                {
+                    x++;
+                    y=0;
+                }
+            }
+        }
+    } 
+    return pow(-1.0,row+col)*det(orden,sub);
+}
+
+void transpose(int size,int array[size][size],int trArray[size][size])
+{
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            trArray[i][j]=array[j][i];
+            printf("%d ",trArray[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
